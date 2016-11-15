5331b3e992d298413f9f7df9b45f7213ff8daab4
tiagocsantos <tiagoc_santos@hotmail.com>
2016-11-15 23:51:58 +0000

    Garantia de Entrega
    envio de mensagem
    resosto com Acks

--- Client/src/Bank/Client/Client.java
+++ Client/src/Bank/Client/Client.java
@@ -1,12 +1,15 @@
+package Bank.Client;
+
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 
 public class Client {
-	
-	private final static int PORT = 8080;
+	private static final int ServerPort = 10100;
+	private static final String ServerHost = "localhost";
 
 	public static void main(String[] args) {
 		System.out.println("Client started running...");
@@ -24,13 +27,35 @@ public class Client {
 	}
 
 	private static void sendMessage(String input) throws Exception {
-		InetAddress addr = InetAddress.getLocalHost();
+		InetAddress addr = InetAddress.getByName(ServerHost);
 		DatagramSocket socket = new DatagramSocket();
-		byte[] buffer = new byte[120];
-		buffer = input.getBytes();
-		DatagramPacket packet = new DatagramPacket(buffer,buffer.length, addr, PORT);
-		socket.send(packet);
 		
-	}
+		byte[] msgbytes = new byte[120];
+		msgbytes = input.getBytes();
+		
+		DatagramPacket packet = new DatagramPacket(msgbytes,msgbytes.length, addr, ServerPort);
+		boolean ackRecived = false;
 
+		byte[] ack = new byte[2];
+        DatagramPacket ackpacket = new DatagramPacket(ack, ack.length);
+
+		while (!ackRecived) {
+			socket.send(packet);
+			System.out.println("Sent message, waiting for ack");
+			
+			try {
+				socket.setSoTimeout(1000);
+				socket.receive(ackpacket);
+				//TODO Parse ack received to filter erro messages sent from the server
+				ackRecived = true;
+				System.out.println("ACK Recived");
+				break;
+			}
+			catch (SocketTimeoutException e){
+				System.out.println("timeout expired re-sending the message");
+				ackRecived = false;
+			}
+		}
+		socket.close();
+	}
 }
--- Server/src/Bank/Server/Server.java
+++ Server/src/Bank/Server/Server.java
@@ -1,20 +1,40 @@
+package Bank.Server;
+
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 
 public class Server {
-
-	private final static int PORT = 8080;
+	private static DatagramSocket socket;
+	
 	public static void main(String[] args) throws Exception {
 		System.out.println("Server started running");
 		byte[] buffer = new byte[120];
-		DatagramSocket socket = new DatagramSocket(PORT);
+		socket = new DatagramSocket(10100);
 		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+		
+		System.out.println("Running - Awaiting Messages");
+		System.out.println("Press enter to shutdown");
+		
 		while(true){
 			socket.receive(packet);
 			String out = new String(packet.getData());
-			System.out.println(out);
+			System.out.println("Recived: "+ out);
+			//Thread.sleep(1000);
+			sendAck(packet.getAddress(), packet.getPort());
+		
 		}
 	}
+	
+	public static void sendAck(InetAddress address, int port) throws IOException{
+	    byte[] ackPacket = new byte[2];
+	    ackPacket[0] = (byte)(1);
+	    ackPacket[1] = (byte)(1);
+	    DatagramPacket acknowledgement = new  DatagramPacket(ackPacket, ackPacket.length, address, port);
+	    socket.send(acknowledgement);
+	    System.out.println("Sent ack");
+}
 
 }
