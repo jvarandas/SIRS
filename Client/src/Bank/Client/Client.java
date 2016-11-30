76b41f3e31d8dfa60af045e36f1f42fa14227dd9
tiagocsantos <tiagoc_santos@hotmail.com>
2016-11-30 01:42:20 +0000

    IDS + Timestamps
    penso que com isto a solução basica fica feita

--- Client/src/Bank/Client/Client.java
+++ Client/src/Bank/Client/Client.java
@@ -1,15 +1,29 @@
 package Bank.Client;
 
+import java.math.BigInteger;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
 
 public class Client {
 	private static final int ServerPort = 10100;
 	private static final String ServerHost = "localhost";
+	
+	private static byte[] Confirmation_Ack = 	new byte[] {0,0};	//transfer completed
+	private static byte[] Amount_Error_Ack = 	new byte[] {1,1};	//amount not available 
+	private static byte[] Source_Unkown_Ack = new byte[] {1,0};		//source iban not found
+	private static byte[] Dest_Unkown_Ack = 	new byte[] {0,1};	//dest iban not fount
+	private static byte[] Not_Autorized_Ack = new byte[] {2,0};		//source iban doesnt belong to that port
+	
+	private static SecureRandom randomizer = new SecureRandom();
 
 	public static void main(String[] args) {
 		System.out.println("Client started running...");
@@ -31,11 +45,17 @@ public class Client {
 	private static void sendMessage(String input) throws Exception {
 		InetAddress addr = InetAddress.getByName(ServerHost);
 		DatagramSocket socket = new DatagramSocket();
+	
+		long timestamp = new Date().getTime();
 		
-		byte[] msgbytes = new byte[120];
-		msgbytes = input.getBytes();
 		
-		DatagramPacket packet = new DatagramPacket(msgbytes,msgbytes.length, addr, ServerPort);
+		long id =  new BigInteger(64, randomizer).longValue(); //generate a 64 bits id for the message , the probability of colision may be ignored(?)
+		
+		String completeMessage = input + " " + timestamp + " " + id;
+				
+		byte[] msgBytes = completeMessage.getBytes();
+
+		DatagramPacket packet = new DatagramPacket(msgBytes,msgBytes.length, addr, ServerPort);
 		boolean ackRecived = false;
 
 		byte[] ack = new byte[2];
@@ -54,12 +74,17 @@ public class Client {
 				
 				//Process info from the ack
 				byte[] ackbytes = ackpacket.getData();
-				if (ackbytes[0]==0 && ackbytes[1]==0)
-					System.out.println("Transaction is completed");
-				else if (ackbytes[0]==1)
-					System.out.println("Message error: Invalid Destination IBAN");
-				else if (ackbytes[1]==1)
-					System.out.println("Message Error: Unavailable Ammount for transfer");
+				if (compareAck(ackbytes, Confirmation_Ack))
+					System.out.println("Transference Completed");
+				else if (compareAck(ackbytes, Amount_Error_Ack))
+					System.out.println("No Funds Available");
+				else if (compareAck(ackbytes, Source_Unkown_Ack))
+					System.out.println("The Registered Number Does Not Have an Account");
+				else if (compareAck(ackbytes, Dest_Unkown_Ack))
+					System.out.println("Destination Account Not Found");
+				else if (compareAck(ackbytes, Not_Autorized_Ack))
+					System.out.println("You Are Not Authorized To Perform That Action");
+				
 				break;
 			}
 			catch (SocketTimeoutException e){
@@ -80,5 +105,9 @@ public class Client {
 	
 	private static void registerIBAN(String iban) throws Exception{
 		sendMessage("associate "+iban);
+	}
+	
+	private static boolean compareAck(byte[] b1, byte[] b2){
+		return (b1[0]==b2[0] && b1[1]==b2[1]);
 	}
 }
--- Server/src/Bank/Server/Server.java
+++ Server/src/Bank/Server/Server.java
@@ -13,6 +13,8 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketAddress;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -24,26 +26,60 @@ public class Server {
 	private static HashMap<SocketAddress, String> Contacts = new HashMap<SocketAddress, String>(); //Associated addrs for each account
 	private static HashMap<String,List<String>> ClientsMatrix = new HashMap<String,List<String>>();
 	private static HashMap<String, String> Passwords = new HashMap<String, String>();
+	private static List<Long> ID_Bucket = new ArrayList<Long>();  //to save the received IDS
+	
+	private static byte[] Confirmation_Ack = 	new byte[] {0,0};	//transfer completed
+	private static byte[] Amount_Error_Ack = 	new byte[] {1,1};	//amount not available 
+	private static byte[] Source_Unkown_Ack = new byte[] {1,0};		//source iban not found
+	private static byte[] Dest_Unkown_Ack = 	new byte[] {0,1};	//dest iban not fount
+	private static byte[] Not_Autorized_Ack = new byte[] {2,0};		//source iban doesnt belong to that port
+	
+	private static long Max_Time_Diff = 2000; //max difference of 2 seconds
 	
 	public static void main(String[] args) throws Exception {
 		System.out.println("Server started running");
 		_config = new File("bank.cnf");
+	
 		byte[] buffer = new byte[120];
 		socket = new DatagramSocket(10100);
+		
 		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+		byte[] ackToSend;
 		
 		System.out.println("Running - Awaiting Messages");
 		System.out.println("Press enter to shutdown");
 		
 		while(true){
+			ackToSend = Not_Autorized_Ack; //by default
 			socket.receive(packet);
-			String out = new String(packet.getData());
-			System.out.println("Received: "+ out);
+			String message = new String(packet.getData());
+			System.out.println("Received: "+ message);
 			//Thread.sleep(1000);
-			byte[] ackToSend = parseMessage(out, packet.getSocketAddress());
+			if (validateTimestamp(message) && validateID(message)){
+				ackToSend = parseMessage(message, packet.getSocketAddress());
+			}
 			sendAck(packet.getAddress(), packet.getPort(), ackToSend);
 		
 		}
+	}
+	
+	private static boolean validateTimestamp(String msg){
+		long timestamp = Long.parseLong(msg.split(" ")[2].trim());
+		long current_time = new Date().getTime();
+		
+		if (current_time-timestamp > Max_Time_Diff)
+			return false;
+		else 
+			return true;
+	}
+	
+	private static boolean validateID(String msg){
+		long id = Long.parseLong(msg.split(" ")[3].trim());
+		 if (!ID_Bucket.contains(id)){
+			 ID_Bucket.add(id);
+			 return true;
+		 }
+		 else return false;
 	}
 	
 	private static byte[] parseMessage(String msg, SocketAddress sender) throws IOException{
@@ -51,13 +87,10 @@ public class Server {
 			String iban = msg.split(" ")[1].trim();			//Since client ports are not fixed we need to register them at the beginning of each run
 			MatrixCard d = new MatrixCard(iban);
 			String password = msg.split(" ")[2];
-			byte[] ackPacket = new byte[2];
 			
 			if(existsIn(iban)){
 				System.out.println("That iban exists associated with another account");
-				ackPacket[0] = 1;
-				ackPacket[1] = 1;
-				return ackPacket;
+				return Not_Autorized_Ack;
 			}
 			
 			Passwords.put(iban, password);
@@ -67,9 +100,7 @@ public class Server {
 			
 			confFile(Passwords, Bank, Contacts);
 			
-			ackPacket[0] = (byte)(0);
-		    ackPacket[1] = (byte)(0);
-			return  ackPacket;
+			return  Confirmation_Ack;
 		}
 				
 			
@@ -79,7 +110,7 @@ public class Server {
 		int amount = Integer.parseInt(info[1].trim());
 		//get IBAN from sender addr
 		String sourceAccount = Contacts.get(sender);
-		return processTransfer(sourceAccount, destAccount, amount); //TODO try and catch
+		return processTransfer(sourceAccount, destAccount, amount);
 
 	}
 	
@@ -146,24 +177,23 @@ public class Server {
 		return false;
 	}
 	
-	private static byte[] processTransfer(String source, String dest, int ammount) throws IOException{ //TODO throw excetions 
+	private static byte[] processTransfer(String source, String dest, int ammount) throws IOException{
 	    
-		byte[] ackPacket = new byte[2];
-	    
-		if (!Bank.containsKey(dest)){
-			ackPacket[0] = (byte)(1);
-		}
-		else if (Bank.get(source)<ammount){
-			ackPacket[1]= (byte)(1);
-		}
+		if (!Bank.containsKey(dest))
+			return Dest_Unkown_Ack;
+		
+		else if (!Bank.containsKey(source))
+			return Source_Unkown_Ack;
+		
+		else if (Bank.get(source)<ammount)
+			return Amount_Error_Ack;
+		
 		else {
 			Bank.put(source, Bank.get(source)-ammount);
 			Bank.put(dest, Bank.get(dest)+ammount);
-			ackPacket[0] = (byte)(0);
-		    ackPacket[1] = (byte)(0);
 		    confFile(Passwords, Bank, Contacts);
+		    return Confirmation_Ack;
 		}
-		return ackPacket;
 	}
 	
 	private static void sendAck(InetAddress address, int port, byte[] ackPacket) throws IOException{
