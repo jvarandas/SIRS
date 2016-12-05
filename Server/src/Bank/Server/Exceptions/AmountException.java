754abc7e767a399d58864c33d9aafcc8fa4c301e
DESKTOP-JFDA5TF\joao <jpedroperalta@hotmail.com>
2016-12-05 14:47:38 +0000

    Project files restructuted

--- Server/src/Bank/Server/Exceptions/AmountException.java
+++
@@ -1,9 +0,0 @@
-package Bank.Server.Exceptions;
-
-public class AmountException extends Exception {
-
-	public AmountException(String message) {
-		super("Amount " + message + " is incorrect");
-	}
-
-}
